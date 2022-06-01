cd83e4c50e41b408e4f2b6fb7f58888e78b47dda
Elliot Potter <58124535+averageflamethrowerguy@users.noreply.github.com>
2022-05-31 22:09:09 -0400

    Add additional folder and build target for the smarter tree

---
+++ cuda/theNextWeek/smarter-trees/objects/hittable.cuh
@@ -0,0 +1,26 @@
+//
+// Created by smooth_operator on 5/17/22.
+//
+
+#ifndef CUDA_HITTABLE_CUH
+#define CUDA_HITTABLE_CUH
+
+#include "../ray.cuh"
+#include "../materials/material.cuh"
+#include "aabb.cuh"
+
+class hittable {
+  public:
+    __device__ virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const = 0;
+    __device__ virtual int getObjectNumber() {
+      return 0;
+    }
+    __device__ virtual bool bounding_box(float time0, float time1, aabb& output_box) const = 0;
+
+    __device__ virtual void print(int depth) {
+      printf("0\n");
+    }
+};
+
+
+#endif //CUDA_HITTABLE_CUH
